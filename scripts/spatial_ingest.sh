402e63e6895434cb627a7c78001d7766b4cc473e
manavnurs14 <manavnursmooloo23@gmail.com>
2026-06-13 04:41:11 +1000

    Update spatial_ingest.sh

--- scripts/spatial_ingest.sh
+++ scripts/spatial_ingest.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+# =========================================================================
+# Programmatic Mesh Block Shapefile Ingestion using ogr2ogr
+# =========================================================================
 
+ogr2ogr -f "PostgreSQL" \
+  PG:"host=localhost port=5435 dbname=ptv-db user=postgres password=postgres" \
+  ./extdata/mb2021.shp \
+  -nln ptv.mb2021 \
+  -nlt PROMOTE_TO_MULTI \
+  -lco GEOMETRY_NAME=geom \
+  -lco FID=id \
+  -overwrite
